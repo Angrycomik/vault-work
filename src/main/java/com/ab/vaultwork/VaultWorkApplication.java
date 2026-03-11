d044ac1308e541de82d350f668d71a095b1d4ce2
Angrycomik <angrycomik@gmail.com>
2026-03-11 13:48:34 +0100

    init

---
+++ src/main/java/com/ab/vaultwork/VaultWorkApplication.java
@@ -0,0 +1,13 @@
+package com.ab.vaultwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaultWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VaultWorkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ab/vaultwork/VaultWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ab.vaultwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaultWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
